49fa68d8abd801643e4fd9e3b73b72c664c7f0c7
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-03-08 13:08:28 +0200

    080323

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from '../Modal/Modal';
 import Searchbar from '../Searchbar/Searchbar';
 import ImageGallery from '../ImageGallery';
